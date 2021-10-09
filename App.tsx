649d0aa4c6962724bc1c3fb0776bb4f4630e325a
Carlos Henrique de Lima <carlos334lima@gmail.com>
2021-10-09 15:28:13 -0300

    fonts

--- App.tsx
+++ App.tsx
@@ -75,7 +75,7 @@ const App = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            Edit <Text style={{ fontFamily:'Poppins-Bold'}}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
